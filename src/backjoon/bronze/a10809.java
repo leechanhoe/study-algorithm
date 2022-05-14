f07ac964d003967fba17ccc6b47b36b1cb7a1d0e
leechanhoe <54972898+leechanhoe@users.noreply.github.com>
2022-05-14 22:02:28 +0900

    study algorithm 22.5.14

---
+++ src/backjoon/bronze/a10809.java
@@ -0,0 +1,26 @@
+package backjoon.bronze;
+
+import java.io.*;
+
+public class a10809 {
+    public static void main (String[] args) throws java.lang.Exception
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int alphabet[] = new int[26];
+        for(int i = 0;i < 26;i++)
+            alphabet[i] = -1;
+
+        int j = 0;
+        for(byte value : br.readLine().getBytes()){
+            if(alphabet[value - 'a'] == -1)
+                alphabet[value - 'a'] = j;
+            j++;
+        }
+
+        for(int i = 0;i < 26;i++)
+            bw.write(alphabet[i] + " ");
+        bw.flush();
+    }
+}
