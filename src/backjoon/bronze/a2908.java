30e0cbdc24c42ad9b854523684f0eeda2141f7d8
leechanhoe <54972898+leechanhoe@users.noreply.github.com>
2022-05-15 19:12:54 +0900

    study algorithm 22.5.15

---
+++ src/backjoon/bronze/a2908.java
@@ -0,0 +1,23 @@
+package backjoon.bronze;
+import java.io.*;
+
+public class a2908 {
+
+    public static void main (String[] args) throws java.lang.Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringBuffer sb = new StringBuffer(br.readLine());
+        String reverse = sb.reverse().toString();
+
+        int a = Integer.parseInt(reverse.split(" ")[0]);
+        int b = Integer.parseInt(reverse.split(" ")[1]);
+
+        if(a > b)
+            bw.write(a + "");
+        else if(a < b)
+            bw.write(b + "");
+
+        bw.flush();
+    }
+}
