ccbd559199357822ca1909832742345ccdf57209
leechanhoe <54972898+leechanhoe@users.noreply.github.com>
2022-05-25 21:38:01 +0900

    study algorithm 22.5.25

---
+++ src/backjoon/silver/silver5/a11650.java
@@ -0,0 +1,36 @@
+package backjoon.silver.silver5;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class a11650 {
+    public static void main(String[] args) throws java.lang.Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int[][] arr = new int[N][2];
+
+        for(int i = 0;i < N;i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, (e1, e2) -> {
+            if(e1[0] == e2[0])
+                return e1[1] - e2[1];
+            else
+                return e1[0] - e2[0];
+        });
+
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0;i < N;i++)
+            sb.append(arr[i][0]).append(' ').append(arr[i][1]).append('\n');
+        bw.write(sb.toString());
+        bw.flush();
+    }
+}
