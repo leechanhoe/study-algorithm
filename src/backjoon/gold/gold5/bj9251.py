bebda31566faacba784e44c81eb4eb38d9ca1415
leechanhoe <54972898+leechanhoe@users.noreply.github.com>
2022-06-20 21:54:00 +0900

    Study DP 22.6.20

---
+++ src/backjoon/gold/gold5/bj9251.py
@@ -0,0 +1,12 @@
+a = input()
+b = input()
+cache = [0] * len(b)
+
+for i in range(len(a)):
+    ps = 0
+    for j in range(len(b)):
+        if ps < cache[j]:
+            ps = cache[j]
+        elif a[i] == b[j]:
+            cache[j] = ps + 1
+print(max(cache))
