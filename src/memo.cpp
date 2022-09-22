2e545235bcf981286ea9f8160cdcfefa3bee344a
leechanhoe <54972898+leechanhoe@users.noreply.github.com>
2022-09-22 21:44:05 +0900

    Study geometry 22.9.22

--- src/memo.cpp
+++ src/memo.cpp
@@ -1,10 +1,17 @@
-#include <iostream>
-#include <string>
-using namespace std;
+#include <stdio.h>
 
-int main(){
-    string a;
-    cin >> a;
-    a[2] = 'a';
-    cout << a;
+int main()
+{
+    int a = 32815, i;
+    // printf("%x %X\n", ((char *)&a), *((char *)&a));
+    // printf("%x %X\n", ((char *)&a + 1), *((char *)&a + 1));
+    // printf("%x %X\n", ((char *)&a + 2), *((char *)&a + 2));
+    // printf("%x %X\n", ((char *)&a + 3), *((char *)&a + 3));
+    // printf("\n%X", a);
+
+    char *p = (char *)&a;
+    for (i = 0; i < 4; i++)
+    {
+        printf("%X ", *(p + i));
+    }
 }
