a8fbad2db283e8c191dd65eba0f45e5f90179f70
leechanhoe <54972898+leechanhoe@users.noreply.github.com>
2023-04-02 21:29:52 +0900

    study java Long 23.4.2

---
+++ src/backjoon/bronze/bronze5/bj11382.java
@@ -0,0 +1,13 @@
+package backjoon.bronze.bronze5;
+import java.util.Scanner;
+public class bj11382
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        long a = sc.nextLong();
+        long b = sc.nextLong();
+        long c = sc.nextLong();
+        System.out.println(a + b + c);
+    }
+}
