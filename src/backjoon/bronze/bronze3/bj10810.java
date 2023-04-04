5873ea554233808db5b39cda822e1fc1785c3125
leechanhoe <54972898+leechanhoe@users.noreply.github.com>
2023-04-04 22:18:00 +0900

    study java array 23.4.4

---
+++ src/backjoon/bronze/bronze3/bj10810.java
@@ -0,0 +1,24 @@
+package backjoon.bronze.bronze3;
+
+public class bj10810 {
+    public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		int[] arr = new int[N];
+		
+		for(int i = 0; i < M; i++){
+		    int start = sc.nextInt();
+		    int end = sc.nextInt();
+		    int ball = sc.nextInt();
+		    
+		    for(int j = start - 1; j < end; j++){
+		        arr[j] = ball;
+		    }
+		}
+		
+		for(int i = 0; i < N; i++){
+		    System.out.print(arr[i] + " ");
+		}
+	}
+}
