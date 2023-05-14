1f9215f4c42bdd8d7dfc1ac8fd295f3f5b413b7d
Chan <54972898+leechanhoe@users.noreply.github.com>
2023-05-14 14:11:43 +0900

    study string 23.5.14

--- src/memo.cpp
+++ src/memo.cpp
@@ -1,48 +1,52 @@
-#include<stdio.h>
-#include<bits/stdc++.h>
+#include <iostream>
+#include <queue>
 using namespace std;
 
-struct Grid{
-	int x;
-	int y;
-};
+int N, G, M;
+pair<int, int> oil[10001];
 
-int N;
-Grid grid[51];
-map<int, int> m;
-
-int getDist(int x1,int y1,int x2,int y2){
-	return pow(abs(x1 - x2), 2) + pow(abs(y1 - y2), 2);
-}
-
-int main()
-{   //프로그램의 시작부 
-	cin >> N;
-	for(int i = 0; i < N; i++){
-		cin >> grid[i].x >> grid[i].y;
+int main() {
+	cin >> N >> G >> M;
+	
+	for(int i = 0; i < M; i++){
+		cin >> oil[i].first;
+	}
+	for(int i = 0; i < M; i++){
+		cin >> oil[i].second;
 	}
 	
-	for(int i = 0; i < N; i++){
-		for(int j = i + 1; j < N; j++){
-			int dist = getDist(grid[i].x, grid[i].y, grid[j].x, grid[j].y);
-			if(m.find(dist) == m.end()){
-				m[dist] = 1;
-			}
-			else{
-				m[dist]++;
-			}
-		}
+	if(N <= G){
+		cout << 0;
+		return 0;
 	}
 	
 	int ans = 0;
-	for(auto a : m){
-		if(a.second >= 4){
-			ans = max(ans, a.first);
+	priority_queue<int> pq;
+	for(int i = 0; i < M; i++){
+		if(oil[i].first <= G)
+			pq.push(oil[i].second);
+		if(oil[i].first > G){
+			while(!pq.empty() && oil[i].first > G){
+				// cout << "1 : "<< pq.top() << '\n';
+				G += pq.top();
+				pq.pop();
+				ans++;
+			}
+			if(oil[i].first > G){
+				ans = -1;
+				break;
+			}
 		}
 	}
-	
-	cout << fixed;
-	cout.precision(2);
-	cout << (double)ans;
-    return 0;
+	while(!pq.empty() && N > G){
+		// cout << "2 : "<< pq.top() << '\n';
+		G += pq.top();
+		pq.pop();
+		ans++;
+	}
+	if(N > G)
+		cout << -1;
+	else
+		cout << ans;
+	return 0;
 }
