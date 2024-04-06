1bc5a4aefdc42a42e24d64c237daf3341f7b73e8
leechanhoe <klavier0823@naver.com>
2024-04-06 10:37:41 +0900

    study string 24.4.6

---
+++ src/backjoon/bronze/bronze4/bj30402.py
@@ -0,0 +1,11 @@
+photo = [input() for _ in range(15)]
+for row in photo:
+    if 'w' in row:
+        print('chunbae')
+        exit(0)
+    elif 'b' in row:
+        print('nabi')
+        exit(0)
+    elif 'g' in row:
+        print('yeongcheol')
+        exit(0)
